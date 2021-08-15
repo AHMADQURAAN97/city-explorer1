5b2be60818f9d7aa4b4ec3fcf14e96e724c8476e
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-08-15 17:20:50 +0300

    df

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+constructor(props){
+super(props);
+
+this.state = {
+cityDate:{},
+searchCity:'',
+showDate:false
 }
+}
+
+getLocation = async (e) => {
+
+  e.preventDefault();
+
+
+}
+
+
+
+render(){
+return (
+
+<div>
+<form onSubmit={this.getLocation}>
+            <input type='text' placeholder='Enter city' name='city' />
+            <button>Explore!</button>
+          </form>
+</div>
+
+
+)
+}
+}
+
 
 export default App;
