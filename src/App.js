daac155b5018ca70785d62c6ac1290d8fe4ae126
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-08-15 20:02:40 +0300

    sdf

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class App extends React.Component {
+
 constructor(props){
 super(props);
 
 this.state = {
-cityDate:{},
+cityData:{},
 searchCity:'',
-showDate:false
+showData:false
 }
 }
 
-getLocation = async (e) => {
+selectLocation = async (e) => {
 
   e.preventDefault();
 
+  await this.setState ({
 
+    searchCity:e.target.city.value
+
+
+  })
+
+let placeURL = `https://eu1.locationiq.com/v1/search.php?key=pk.b72f636506faa3cf5c9f31dbb231730b&q=irbid&format=json`
+
+let resultData = await axios.get(placeURL)
+
+
+this.setState({
+cityData:resultData.data[0],
+showData:true
+})
 }
 
 
@@ -25,10 +44,24 @@ render(){
 return (
 
 <div>
-<form onSubmit={this.getLocation}>
-            <input type='text' placeholder='Enter city' name='city' />
-            <button>Explore!</button>
-          </form>
+  <h1>City-Explorer</h1>
+
+   <form onSubmit={this.selectLocation} class="form-group">
+  
+    <input type="text" class="form-control" placeholder='Enter city' name='city' />
+    <button type="submit" class="btn btn-primary">Explore!</button>
+     </form>
+
+
+
+{this.state.showData && 
+<p>{this.state.searchCity} <br/>
+Latitude : {this.state.cityData.lat} <br/>
+ Longitude : {this.state.cityData.lon} </p>
+
+}
+
+
 </div>
 
 
