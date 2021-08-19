37db7281c29b1c17c862d9ccb6eecf0ddb31b277
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-08-19 18:34:11 +0300

    sepirt component

---
+++ src/components/weatherDay.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { Table } from "react-bootstrap";
+
+// class weatherDay extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Table striped bordered hover>
+//           <tbody key={this.props.key}>
+//             <tr>
+//               <th>Date: {this.props.date}</th>
+//               <th>description: {this.props.description}</th>
+//             </tr>
+//           </tbody>
+//         </Table>
+//       </div>
+//     );
+//   }
+// }
+
+// export default weatherDay;
